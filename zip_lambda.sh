2482602f59551b6829805e474dc9d8afa6ad8343
Denis Gaina <denis.gaina@endava.com>
2023-10-12 10:34:30 +0300

    use variables for convenience

--- zip_lambda.sh
+++ zip_lambda.sh
@@ -2,16 +2,21 @@
 
 set -e
 
+# replace the value with your S3 bucket name
+S3_BUCKET_NAME="denis-gaina-cloudformation"
+CF_STACK_NAME="denis-gaina-stack"
+AWS_PROFILE_NAME="default"
+
 # archiving code
 zip code.zip ./code.py
 
 # updating s3 with updated code
 # comment out the below line if your aws profile in the credentials file is not named this way
-export AWS_PROFILE=default
-aws s3 sync . s3://denis-gaina-cloudformation/lambda
+export AWS_PROFILE=$AWS_PROFILE_NAME
+aws s3 sync . s3://$S3_BUCKET_NAME/lambda
 
 # validating template
-CF_VALIDATION=$(aws cloudformation validate-template --template-url https://denis-gaina-cloudformation.s3.us-east-1.amazonaws.com/lambda/lambda.yaml)
+CF_VALIDATION=$(aws cloudformation validate-template --template-url https://$S3_BUCKET_NAME.s3.us-east-1.amazonaws.com/lambda/lambda.yaml)
 echo $CF_VALIDATION
 
 # extracting capabilities from validation
@@ -19,4 +24,4 @@ CAPABILITIES=$(echo $CF_VALIDATION | jq -r '.Capabilities[]')
 
 # creating stack
 DATEANDTIME=$(date '+%Y-%m-%d-%H-%M-%S')
-aws cloudformation create-stack --stack-name denis-gaina-stack-$DATEANDTIME --template-url https://denis-gaina-cloudformation.s3.us-east-1.amazonaws.com/lambda/lambda.yaml --capabilities $CAPABILITIES --region=us-east-1
+aws cloudformation create-stack --stack-name $CF_STACK_NAME-$DATEANDTIME --template-url https://$S3_BUCKET_NAME.s3.us-east-1.amazonaws.com/lambda/lambda.yaml --capabilities $CAPABILITIES --region=us-east-1
