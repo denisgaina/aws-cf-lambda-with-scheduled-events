27af1d6553734fd6bf526cb68a3e36385a1cb9fb
denisgaina <gainadenis7@gmail.com>
2024-10-04 00:24:13 +0300

    Update zip_lambda.sh

--- zip_lambda.sh
+++ zip_lambda.sh
@@ -13,10 +13,10 @@ zip code.zip ./code.py
 # updating s3 with updated code
 # comment out the below line if your aws profile in the credentials file is not named this way
 export AWS_PROFILE=$AWS_PROFILE_NAME
-aws s3 sync . s3://$S3_BUCKET_NAME/lambda
+aws s3 sync . s3://$S3_BUCKET_NAME/lambda --region=us-east-1
 
 # validating template
-CF_VALIDATION=$(aws cloudformation validate-template --template-url https://$S3_BUCKET_NAME.s3.us-east-1.amazonaws.com/lambda/lambda.yaml)
+CF_VALIDATION=$(aws cloudformation validate-template --template-url https://$S3_BUCKET_NAME.s3.us-east-1.amazonaws.com/lambda/lambda.yaml --region=us-east-1)
 echo $CF_VALIDATION
 
 # extracting capabilities from validation
@@ -25,3 +25,6 @@ CAPABILITIES=$(echo $CF_VALIDATION | jq -r '.Capabilities[]')
 # creating stack
 DATEANDTIME=$(date '+%Y-%m-%d-%H-%M-%S')
 aws cloudformation create-stack --stack-name $CF_STACK_NAME-$DATEANDTIME --template-url https://$S3_BUCKET_NAME.s3.us-east-1.amazonaws.com/lambda/lambda.yaml --capabilities $CAPABILITIES --region=us-east-1
+
+# deleting stack
+# aws cloudformation delete-stack --stack-name $CF_STACK_NAME-$DATEANDTIME --region=us-east-1
